4a0b50d01b1ad2955ad55717026d3c62befc444f
unknown <aldrin.brinas@frontlinebusiness.com.ph>
2024-10-09 07:51:53 +0800

    header 100%

--- dist/js/toggle_menu.js
+++ dist/js/toggle_menu.js
@@ -1,5 +1,5 @@
 const burgir = document.querySelector(".toggle_menu");
-const headernav = document.querySelector("ul.header_nav");
+const headernav = document.querySelector("ul.header_nav a.navA");
 burgir.addEventListener("click", () => {
     burgir.classList.toggle("open");
     headernav.classList.toggle("open");
