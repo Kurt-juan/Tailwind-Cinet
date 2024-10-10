77e13bbda6305b2b95ea4b62366b37be3ca339fa
unknown <aldrin.brinas@frontlinebusiness.com.ph>
2024-10-10 15:59:39 +0800

    banner incomplete

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,9 +10,13 @@ module.exports = {
         "accent": "#ffc139",
       },
       backgroundImage:{
-        "banner-desk":"url('/dist/img/cinet-banner-desktop.svg')"
+        "banner-desk":"url('/dist/img/cinet-banner-desktop.svg')",
+        "cat-1":"url('/dist/img/destinations-1-363x260.jpg')",
+        "cat-2":"url('/dist/img/destinations-2-363x260.jpg')",
+        "cat-3":"url('/dist/img/destinations-3-363x260.jpg')",
+        "cat-4":"url('/dist/img/destinations-4-363x260.jpg')",
+        "cat-5":"url('/dist/img/destinations-5-363x260.jpg')",
       },
-   
     },
   },
   plugins: [],
